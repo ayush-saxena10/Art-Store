1df00c14d9e3156f845e05c77fb4c723975ec80d
Ayush Saxena <ayushsaxena221999@gmail.com>
2022-07-06 20:49:20 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// Button Toggler
+
+
+
+// Carousel
+buttons = document.querySelectorAll("[data-carousel-button]")
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    let offset = 0
+    if (button.dataset.carouselButton === "next") offset = 1
+    else offset = -1
+
+    const slides = button.closest("[data-carousel]").querySelector("[data-slides]")
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide)+offset
+    if (newIndex<0) newIndex = slides.children.length-1
+    if (newIndex >= slides.children.length) newIndex = 0
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
+})
+
+function autoCarousel() {
+  const slides = document.querySelector("[data-slides]")
+  const activeSlide = slides.querySelector("[data-active]")
+  let newIndex = [...slides.children].indexOf(activeSlide)+1
+  if (newIndex >= slides.children.length) newIndex = 0
+  slides.children[newIndex].dataset.active = true
+  delete activeSlide.dataset.active
+}
+
+setInterval(autoCarousel, 5000)
